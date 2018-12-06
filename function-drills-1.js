e9589ec6d93b578d92993f7bef237df23a69545d
Jordan Epps <jordanepps@gmail.com>
2018-12-06 11:13:55 -0500

    Add error checking to createGreeting function for the parameters

--- function-drills-1.js
+++ function-drills-1.js
@@ -4,9 +4,13 @@ function getYearofBirth(age) {
 }
 
 function createGreeting(name, age) {
+  if (!name || !age) throw new Error('Arguments not valid');
+  if (typeof name !== 'string') throw new TypeError('Argument is not a string');
+  if (typeof age !== 'number') throw new TypeError('Argument is NaN');
   const yearOfBirth = getYearofBirth(age);
   return `Hi, my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}.`;
 }
+
 try {
   const greeting1 = createGreeting('Jordan', 26);
 } catch (e) {
