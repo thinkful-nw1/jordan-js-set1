98a2624826cd3b474097f9dab580f6531a0901ae
Jordan Epps <jordanepps@gmail.com>
2018-12-06 10:36:48 -0500

    Create yearOFBirth variable and add value to a sentence in the return statement

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,5 +1,6 @@
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age}`;
+  const yearOfBirth = 2018 - age;
+  return `Hi, my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting('Jordan', 26);
